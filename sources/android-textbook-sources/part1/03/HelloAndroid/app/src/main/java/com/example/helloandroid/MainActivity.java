a8a2140d5c7cc08994fbc75277ef2908fa847feb
PARK <satcop@naver.com>
2018-07-01 12:54:44 +0900

    소스커밋

--- sources/android-textbook-sources/part1/03/HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.java
+++ sources/android-textbook-sources/part1/03/HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // SET !!!
 
         // set Content View
         setContentView(R.layout.activity_main);
